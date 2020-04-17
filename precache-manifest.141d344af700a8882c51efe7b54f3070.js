ac11091b8b7360cc60f0462003937fef8e0f90e6
project-William <williamms669@gmail.com>
2020-04-16 21:49:16 -0500

    Updates

---
+++ precache-manifest.141d344af700a8882c51efe7b54f3070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c7d848c1236e1d450fb37b7748c1fe",
+    "url": "/covid19-react-app/index.html"
+  },
+  {
+    "revision": "f927e9f6ff922db12a18",
+    "url": "/covid19-react-app/static/css/main.152f8b52.chunk.css"
+  },
+  {
+    "revision": "6053467009ee10dc47a7",
+    "url": "/covid19-react-app/static/js/2.bdc9cac8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-react-app/static/js/2.bdc9cac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f927e9f6ff922db12a18",
+    "url": "/covid19-react-app/static/js/main.491e3743.chunk.js"
+  },
+  {
+    "revision": "281fd3cb57f86acb5fe6",
+    "url": "/covid19-react-app/static/js/runtime-main.c802343f.js"
+  }
+]);
