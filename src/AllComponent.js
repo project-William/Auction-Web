96624631f9b6867b657f15974864ba7ac9068719
project-William <williamms669@gmail.com>
2020-04-16 21:52:47 -0500

    delete one chart

--- src/AllComponent.js
+++ src/AllComponent.js
@@ -227,17 +227,6 @@ function Charts({ data,date }) {
 
                 </Grid>
             </Grid>
-
-            <Grid container spacing={1} justify='center'>
-                <Grid item xs={12} md={2}
-                    component={Card}
-                    className={cx(style.card,style.lineCard)}
-                >
-                    <LineChart data={data} />
-
-                </Grid>
-
-            </Grid>
         </main>
     )
 }
